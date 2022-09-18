4a67702193a7a0ea3d145655a285b635ebccb9bb
handle#1337 <104642778+DX9Paster@users.noreply.github.com>
2022-09-18 17:23:31 -0400

    Add files via upload

---
+++ utils/includes.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <random>
+#include <Windows.h>
+#include <string>
+#include "../utils/li.hpp"
+#include "../utils/skCrypt.h"
+#include "../utils/xorstr.hpp"
+#include <thread>
+#include <lmcons.h>
+#include <filesystem>
+#include <fstream>
+#include "../authencation/auth.hpp"
+
+#include <random>
+#include <thread>
+#include <sstream>
+#include <string.h>
+#include <array>
+#include <Iphlpapi.h>
+#include <Assert.h>
+#include <iomanip>
+#include <conio.h>
+#include <intrin.h>
+#include <comdef.h>
+#include <Wbemidl.h>
+#include <TlHelp32.h>
+#include "colors.h"
+#include "../mapper/kdmapper.hpp"
+#include "../UUIDS/biosuuid.h"
+
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "wbemuuid.lib")
+
